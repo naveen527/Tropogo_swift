6060a9403d5ef9825d626f9cbddd51534f135a75
naveen kumar <naveen527.ios@gmail.com>
2021-01-09 17:27:49 +0530

    Update ViewController.swift

--- Tropogo/Tropogo/ViewController.swift
+++ Tropogo/Tropogo/ViewController.swift
@@ -13,23 +13,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         #if Prod
+        
         #if DEBUG
         print("Prod Debug")
         #else
         print("Prod Release")
         #endif
+        
         #elseif Dev
+        
         #if DEBUG
         print("Dev Debug")
         #else
         print("Dev Release")
         #endif
+        
         #elseif Stage
+        
         #if DEBUG
         print("Stage Debug")
         #else
         print("Stage Release")
         #endif
+        
         #endif
         
         
